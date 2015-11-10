847586118c63176827796090da4331db1913d109
Robert Clugston <robert@clugston.org>
2015-11-10 11:51:18 -0800

    Added Stuff

---
+++ head.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+import os.path
+import web
+import sqlite3
+import threading
+
+####################
+# RESULTS IDENTIFIER
+# 0 = not loaded
+# 1 = loaded no errors
+# 2 = crashed
+# 3 = hung
+
+#### Web API ####
+counter = 0
+lock = threading.Lock()
+
+#class start_test:
+#    def GET(self):
+#        raise web.seeother("/test/"+str(counter)+"/payload")
+
+class get_nextid():
+    def GET(self):
+        return counter
+
+class get_payload():
+    def GET(self,testnum):
+        global counter
+        global lock
+        testcase_tmpl = web.template.frender("testcase.html")
+        counter+=1
+        payload="BLAH"
+        try:
+            lock.acquire(True)
+            cur.execute("INSERT INTO testcases (id,result,payload) VALUES (?,?,?)", (testnum,str(0), payload))
+        finally:
+            lock.release()
+
+        return testcase_tmpl(testnum,payload)
+
+class mark_loaded():
+    def GET(self, testnum):
+        global lock
+        try:
+            lock.acquire(True)
+            #cur = conn.cursor()
+            cur.execute("SELECT count(id) FROM testcases WHERE id = ?", (testnum,))
+            row = cur.fetchone()
+            if (row is not None):
+                cur.execute("UPDATE testcases SET result = 1 WHERE id = ?", (testnum,))
+                conn.commit()
+        finally:
+            lock.release()
+        
+        return counter
+
+class get_status():
+    def GET(self, testnum):
+        global lock
+        try:
+            lock.acquire(True)
+            cur.execute("SELECT result FROM testcases WHERE id = ?", (testnum,))
+            row = cur.fetchone()
+        finally:
+            lock.release()
+        
+        if row is None:
+            return -1
+        else:
+            return row[0]
+
+class record_crash():
+    def POST(self, testnum):
+        pass
+
+urls = (
+    "/getnextid", "get_nextid",
+    "/test/(\d+)/payload", "get_payload",
+    "/test/(\d+)/loaded", "mark_loaded",
+    "/test/(\d+)/status", "get_status"
+    #"/test/(\d+)/record_crash","record_crash"
+)
+
+app = web.application(urls, globals())
+conn = sqlite3.connect("state.db", check_same_thread=False)
+cur = conn.cursor()
+
+def db_setup():
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='testcases';")
+    row = cur.fetchone()
+    if (row is None):
+        cur.execute("CREATE TABLE testcases (id int, result int, payload text)")
+        conn.commit()
+    else:
+        pass
+
+if __name__ == '__main__':
+    db_setup()
+    app.run()
---
+++ node.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+import os,sys
+import subprocess
+import time
+sys.path.append("../pydbg")
+
+from pydbg import *
+global debug
+
+#import http.client
+#import httplib
+import requests
+
+last_testid = 0
+attempts = 0
+
+def m_conmsg(message, indent):
+    sys.stdout.write("  "* indent) 
+    if indent > 0:
+        sys.stdout.write("- "+message+"... ")
+    else:
+        sys.stdout.write("[*] "+message+"... ")
+
+def get_nextid():
+    global last_testid
+    m_conmsg("Getting next test id", 0)
+    r = requests.get("http://localhost:8081/getnextid")
+    last_testid = r.text
+    print last_testid
+
+def not_loaded(testid):
+    global attempts
+    m_conmsg("Checking page loaded", 1)
+    #connection = httplib.HTTPConnection("localhost:8081") 
+    #connection.request('GET','/test/'+str(testid)+"/status")
+    #response = connection.getresponse().read()
+    r = requests.get("http://localhost:8081/test/%s/status" % testid)
+    response = r.text
+    if attempts == 3:
+        print "FAILED TO LOAD"
+        # TODO LOG FAILURE
+        attempts = 0
+        return False
+    elif response == "0":
+        attempts += 1
+        print "NOT LOADED"
+        return True 
+    elif response == "1":
+        attempts = 0
+        print "LOADED"
+        return False
+    elif response == "-1":     
+        print "NOT LOADED"
+        return True
+    else:
+        print "FAILURE"
+        return True
+
+
+def test_loop():
+    start_safari()
+    while(1):
+        load_test()
+
+
+def start_safari():
+    ## TODO
+    # Set Heap Debugging environmental variables
+    debug = pydbg()
+    m_conmsg("Starting Safari",0)
+    safari_process = subprocess.Popen(['/Applications/Safari.app/Contents/MacOS/SafariForWebKitDevelopment', ''], env=dict(os.environ, DYLD_INSERT_LIBRARIES="/usr/lib/libgmalloc.dylib"), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    debug.attach(safari_process.pid)
+    print "DONE"
+    time.sleep(1)
+    m_ascript('tell application "Safari" to close every window') 
+    m_ascript('tell application "Safari" to open location "about:blank"') 
+
+
+def load_test():
+    get_nextid()
+    current_id = last_testid
+    m_ascript('tell application "Safari" to set the URL of the front document to "http://localhost:8081/test/' + str(current_id) + '/payload"')
+    while(not_loaded(current_id)):
+        time.sleep(1)
+
+def m_ascript(ascript):
+    osa = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    return osa.communicate(ascript)[0]
+
+def handler_crash(pydbg):
+    context = pydbg.dump_context()
+
+def main():
+    test_loop()
+
+if __name__ == '__main__':
+    main() 
